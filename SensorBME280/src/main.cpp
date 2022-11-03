5818ca96ed62bc89e13416fd9e86a1abd2b552f9
Vũ Thành Trung <87574476+trungnghien@users.noreply.github.com>
2022-11-03 18:42:52 +0700

    Update main.cpp

--- SensorBME280/src/main.cpp
+++ SensorBME280/src/main.cpp
@@ -9,7 +9,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //bme.run();
-  bme.getData();
-  bme.logData();
+  bme.run();
+  /*bme.getData();
+  bme.logData();*/
 }
